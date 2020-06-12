24c6d53bff078182d3bf5b3566ebfae8328be328
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-12 11:21:24 -0500

    completed mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
+
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  // get Characters from Rick and Morty API
+  useEffect(()=>{
+    axios.get('https://rickandmortyapi.com/api/character/')
+      .then( res =>{
+          setCharacters(res.data.results);  
+      })
+      .catch(err =>{
+        console.log(err);
+    })
+  },[])
+
+
+
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">The World of Rick & Morty</h1>
+      {
+        characters.map( character =>{
+          return <Character characterInfo={character}></Character>
+        })
+      }
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,60 @@
 // Write your Character component here
+
+import React from 'react';
+import styled from 'styled-components'
+
+const CharacterWrapper = styled.div`
+    border: 1px solid #c7fa6c;
+    width: 30%;
+    margin: 10px auto;
+    display: flex;
+    align-items: center;
+    /* box-shadow: 3px 3px 1px 1px #c7fa6c; */
+    box-shadow:
+    1px 0px 1px #c7fa6c,
+    0px 1px 1px #c1f762,
+    2px 1px 1px #c7fa6c,
+    1px 2px 1px #c1f762,
+    3px 2px 1px #c7fa6c,
+    2px 3px 1px #c1f762,
+    4px 3px 1px #c7fa6c,
+    3px 4px 1px #c1f762,
+    5px 4px 1px #c7fa6c,
+    4px 5px 1px #c1f762,
+    6px 5px 1px #c7fa6c;
+
+    &:hover{
+        cursor: pointer;
+        position: relative;
+        top:3px;
+        left:2px;
+        box-shadow: 0px 0px;
+    }
+
+
+    h2{
+        color: #c1f762;
+    }
+   
+`
+
+const Portrait = styled.img`
+    width: 20%;
+    margin: 10px;
+    border-radius: 50%;
+    margin-right: 60px;
+`
+
+function Character(props){
+
+    const character = props.characterInfo
+
+    return(
+        <CharacterWrapper>
+            <Portrait src={character.image}></Portrait>
+            <h2>{character.name}</h2>
+        </CharacterWrapper>
+    )
+}
+
+export default Character
