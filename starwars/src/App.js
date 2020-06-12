ed45f35a7fde3dc3a856bf57ab3f3c0a53077820
antonio martinez baez <tono.mtzb@gmail.com>
2020-06-12 12:31:07 -0500

    added search form

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -8,11 +8,12 @@ const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
   const [characters, setCharacters] = useState([]);
+  const [searchText, setSearchText] = useState('')
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
-  // get Characters from Rick and Morty API
+  // get Characters from Rick and Morty API on first render 
   useEffect(()=>{
     axios.get('https://rickandmortyapi.com/api/character/')
       .then( res =>{
@@ -24,10 +25,21 @@ const App = () => {
   },[])
 
 
+  function searchHandler(e){
+    setSearchText(e.target.value)
+  }
+
 
   return (
     <div className="App">
       <h1 className="Header">The World of Rick & Morty</h1>
+      <form>
+        <input type='text' 
+        placeholder='Search Characters' 
+        className='search'
+        onChange={searchHandler}>
+        </input>
+      </form>
       {
         characters.map( character =>{
           return <Character key={character.id} characterInfo={character}></Character>
